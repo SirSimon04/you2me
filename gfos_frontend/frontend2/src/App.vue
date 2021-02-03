c4ca4a7341e69bf542b46922387e7d0c3d6a29c9
JET3141 <jan.thomas@gymnasium-essen-werden.de>
2021-02-03 23:35:59 +0100

    Änderungen an "Registrieren.vue". Unfertige Version da codepen.io überlastet.

--- gfos_frontend/frontend2/src/App.vue
+++ gfos_frontend/frontend2/src/App.vue
@@ -43,7 +43,8 @@ export default {
   },
 
   data: () => ({
-    //
+  
   }),
+  
 };
 </script>
--- gfos_frontend/frontend2/src/components/Registrieren.vue
+++ gfos_frontend/frontend2/src/components/Registrieren.vue
@@ -1,3 +1,4 @@
+
 <template>
     <v-container>
         <v-stepper v-model="e1">
@@ -58,34 +59,138 @@
           </v-btn>
   
           <v-btn text>
-            Abbrechen
+            Impressum
           </v-btn>
-          <v-checkbox
+          <!--<v-checkbox
             v-model="checkbox"
             :label="'Ich bin bereits registriert'"
-            ></v-checkbox>
+            ></v-checkbox> -->
         </v-stepper-content>
   
         <v-stepper-content step="2">
           <v-card
             class="mb-12"
             color="white"
-            height="200px"
+            height="500px"
           >
+          <subheading>Schritt 2:</subheading>
           <v-text-field
               v-model="vorname"
               :rules="nameRules"
               :counter="15"
-              label="Vornamen eingeben"
+              label="Vornamen eingeben*"
+              required
               
             ></v-text-field>
             <v-text-field
               v-model="nachname"
               :rules="nameRules"
               :counter="15"
-              label="Nachnamen eingeben"
+              label="Nachnamen eingeben*"
+              required
               
             ></v-text-field>
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              :counter="15"
+              label="E-Mail eingeben"
+              
+            ></v-text-field>
+            <v-text-field
+              v-model="info"
+              :rules="nameRules"
+              :counter="256"
+              label="Info eingeben"
+              
+            ></v-text-field>
+            <v-file-input
+               truncate-length="15"
+               label="Profilbild hinzufügen"
+            ></v-file-input>
+            <v-dialog
+        v-model="dialog"
+        width="500"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          
+          <v-btn
+        class="mx-2"
+        
+        dark
+        small
+        color="primary"
+        v-bind="attrs"
+            v-on="on"
+      >
+      
+        <v-icon dark>
+          Hilfe
+        </v-icon>
+        
+      </v-btn>
+        </template>
+  
+        <v-card>
+          <v-card-title class="headline grey lighten-2">
+            Informationen: "Info"
+          </v-card-title>
+  
+          <v-card-text>
+            Vorname:
+            <br>
+            Bitte geben Sie Ihren Vornamen an. Ihr Name wird für keinen Nutzer der App ersichtlich sein, kann aber - im Falle einer strafrechtlichen Verfolgung oder anderen rechtlichen Maßnahmen- an Hoheitsgewalten weitergegeben werden. Dieses Feld ist Pflicht.
+            <br>
+            <br>
+            Nachname:
+            <br>
+            Bitte geben Sie in diesem Feld Ihren Nachnamen an. Das Feld ergänzt das oben stehende Feld "Vorname". Dieses Feld ist Pflicht.
+            <br>
+            <br>
+            Handynummer:
+            <br>
+            Die Handynummer macht es für Sie bei der Nutzung der App leicht, Kontakte zu finden und von Ihren Kontakten gefunden zu werden. Dieses Feld ist nicht Pflicht.
+            <br>
+            <br>
+            E-Mail:
+            <br>
+            Bitte geben Sie in diesem Feld eine E-Mail-Adresse ein, unter der wir Sie im Ernstfall erreichen können und die Zwei-Faktor-Authentifizierung nach der Registrierung durchführbar ist. Dieses Feld ist Pflicht.
+            <br>
+            <br>
+            Info:
+            <br>
+            Die Info (Bio, Status) beschreibt Sie in kurzen Worten. Die Info kann von allen Nutzern der App gesehen werden, sofern diese Ihr Profil aufrufen. Geben Sie bitte keine Personenbezogenen Daten wie Handy- oder Kreditkartennummer an und vermeiden Sie die Nennung Ihres genauen Wohnortes.
+            <br>
+            <br>
+            Profilbild:
+            <br>
+            Das Profilbild ist Ihr Gestaltungsbereich. Hier können Sie sich oder ein schönes Motiv für Ihre Kontakte anzeigen lassen. Die Datenschutzbedingungen zum Profilbild lassen sich ändern. Es ist keine Pflicht, ein Profilbild anzugeben.
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              Verstanden 
+              <v-icon dark>
+              mdi-checkbox-marked-circle
+            </v-icon>
+            </v-btn>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+               > <!-- Allgemein wäre der zweite Knopf als "Link" zu einer Anleitungs-Website, die hilflose Angestellte im Zweifel nutzen können.-->
+              Nähere Informationen
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
           </v-card>
   
           <v-btn
@@ -95,17 +200,19 @@
             Weiter
           </v-btn>
   
-          <v-btn text>
-            Abbrechen
+          <v-btn text
+           @click="e1 = 1">
+            Zurück
           </v-btn>
         </v-stepper-content>
   
         <v-stepper-content step="3">
           <v-card
             class="mb-12"
-            color="grey lighten-1"
+            color="white"
             height="200px"
           >
+          
           <h4>Login</h4>
           <v-text-field
               v-model="Benuztername"
@@ -115,8 +222,8 @@
               required
             ></v-text-field>
             <v-text-field
-              v-model="passwort"
-              :rules="passwordRules"
+              v-model="password"
+              :rules="nameRules"
               label="Passwort"
               required
             ></v-text-field>
@@ -124,27 +231,43 @@
   
           <v-btn
             color="primary"
-            @click="e1 = 1"
+            @click= "einloggen"
           >
-            Continue
+            Login
           </v-btn>
   
-          <v-btn text>
-            Cancel
+          <v-btn text
+          @click= "e1 = 1">
+            Registrieren
           </v-btn>
         </v-stepper-content>
       </v-stepper-items>
     </v-stepper>
     </v-container>
 </template>
+<style>
 
+</style>
+  .group {
+  display: flex;
+  flex: 1;
+  justify-content: space-around;
+  }
 <script>
   export default {
     name: 'Registrieren',
-  }
-  
     
-  
+    data(){
+      return {
+      e1: 3,
+      checkbox: true,
+      dialog: false,
+    }
   
 
+    }
+  }
+  
+ 
+  
 </script>
