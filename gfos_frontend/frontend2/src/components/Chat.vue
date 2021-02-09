a05b803ebaebd46cc75c93d61783a23a6cbd1caf
Lukas Baginski <lukas.baginski@ambilstein.de>
2021-02-09 08:54:43 +0100

    Chat.vue created

---
+++ gfos_frontend/frontend2/src/components/Chat.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">
+          SirSimon04
+        </div>
+        <v-list-item-subtitle>Guten Morgen, ich bin verdammt müde!</v-list-item-subtitle><br>
+        <v-list-item-subtitle>08:53</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Chat',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
