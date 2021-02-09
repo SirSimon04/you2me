77cd32dede9b8b2778cc47b1dc903d592cb71398
Lukas Baginski <lukas.baginski@ambilstein.de>
2021-02-09 09:04:56 +0100

    Vue-Config: Port changed to 80

--- gfos_frontend/frontend2/vue.config.js
+++ gfos_frontend/frontend2/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 80, // CHANGE YOUR PORT HERE!
+        https: false,
+        hotOnly: false,
+    },
+    transpileDependencies: [
+        'vuetify'
+    ]
 }
