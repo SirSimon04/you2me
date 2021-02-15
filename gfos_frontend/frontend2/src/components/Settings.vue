aa993862d1462f899acae1fe8747dff3030765cd
JET3141 <jan.thomas@gymnasium-essen-werden.de>
2021-02-15 21:11:53 +0100

    Aenderungen an Settings.vue

--- gfos_frontend/frontend2/src/components/Settings.vue
+++ gfos_frontend/frontend2/src/components/Settings.vue
@@ -31,67 +31,147 @@
             three-line
             subheader
           >
-            <v-subheader>Benutzereinstellungen</v-subheader>
+            <v-subheader>Angemeldet:</v-subheader>
             <v-list-item>
               <v-list-item-content>
-              
                 <v-card
-                    elevation="2"
-                    
-                    max-width= "400px"
-                    height:= "50px"
-                    outlined
-                ><v-card-text>
-                    <v-list-item-title class="display-5 text--primary">
-                    Erscheinungsbild
-                    </v-list-item-title>
-                    <v-list-item-subtitle><v-btn><p>Ändern Sie das Design der Anwendung.</p></v-btn></v-list-item-subtitle>
-                    </v-card-text>
-                </v-card>
-              
+                          class="mx-auto"
+                          max-width="344"
+                        >
+                          <v-img
+                            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                            height="200px"
+                          ></v-img>
+                      
+                          <v-card-title>
+                            {{benutzername}}
+                          </v-card-title>
+                      
+                          <v-card-subtitle>
+                            {{nachname}}, {{vorname}}<br>
+                            {{email}}
+                          </v-card-subtitle>
+                      
+                          <v-card-actions>
+                            
+                      
+                            <v-spacer></v-spacer>
+                      
+                            <v-btn
+                              icon
+                              @click="show = !show"
+                            >
+                              <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                            </v-btn>
+                          </v-card-actions>
+                      
+                          <v-expand-transition>
+                            <div v-show="show">
+                              <v-divider></v-divider>
+                      
+                              <v-card-text>
+                                {{info}}
+                              </v-card-text>
+                            </div>
+                          </v-expand-transition>
+                        </v-card>
                 
               </v-list-item-content>
             </v-list-item> <!--TODO: Die Einsetellungen sollten noch so angepasst werden, dass sie ein schönes Erscheinungsbild besitzen. Ähnlich Windows 10?-->
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>Account</v-list-item-title>
-                <v-list-item-subtitle>Require password for purchase or use password to restrict purchase</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
+            
           </v-list>
           <v-divider></v-divider>
           <v-list
-            three-line
+            five-line
             subheader
           >
-            <v-subheader>General</v-subheader>
+            <v-subheader>Einstellungen:</v-subheader>
             <v-list-item>
               <v-list-item-action>
-                <v-checkbox v-model="notifications"></v-checkbox>
+                <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  color="blue">
+                    <v-icon dark>
+                      mdi-format-list-bulleted-square
+                    </v-icon>
+                </v-btn>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>Notifications</v-list-item-title>
-                <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                <v-list-item-title>Benachrichtigungseinstellungen</v-list-item-title>
+                <v-list-item-subtitle>Benachrichtigungstöne, besondere Benachrichtigugnen, blockierte Kontakte, ...</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
             <v-list-item>
               <v-list-item-action>
-                <v-checkbox v-model="sound"></v-checkbox>
+                <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  color="blue">
+                    <v-icon dark>
+                      mdi-format-list-bulleted-square
+                    </v-icon>
+                </v-btn>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>Sound</v-list-item-title>
-                <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
+                <v-list-item-title>Toneinstellungen</v-list-item-title>
+                <v-list-item-subtitle>Töne, Stummschaltungen, Mikrofon/Lautsprecher</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
             <v-list-item>
               <v-list-item-action>
-                <v-checkbox v-model="widgets"></v-checkbox>
+                <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  color="blue">
+                    <v-icon dark>
+                      mdi-format-list-bulleted-square
+                    </v-icon>
+                </v-btn>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>Auto-add widgets</v-list-item-title>
-                <v-list-item-subtitle>Automatically add home screen widgets</v-list-item-subtitle>
+                <v-list-item-title>Erscheindungsbild</v-list-item-title>
+                <v-list-item-subtitle>Modus, Helligkeit, Farben, ...</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
+            <v-list-item>
+              <v-list-item-action>
+                <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  color="blue">
+                    <v-icon dark>
+                      mdi-format-list-bulleted-square
+                    </v-icon>
+                </v-btn>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Account</v-list-item-title>
+                <v-list-item-subtitle>Benutzereinstellungen, Datenschutz, Kontaktlisten, ...</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-action>
+                <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  color="blue">
+                    <v-icon dark>
+                      mdi-format-list-bulleted-square
+                    </v-icon>
+                </v-btn>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Informationen</v-list-item-title>
+                <v-list-item-subtitle>Speicherplatz, Hilfe, Entwickleroptionen, ...</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            
           </v-list>
         </v-card>
     </v-container>
@@ -103,6 +183,12 @@
                 data(){
                 return {
                     dialog: false,
+                    show: false,
+                    benutzername: "SirSimon",
+                    nachname: "Engel",
+                    vorname: "Simon",
+                    email: "simi@engelnet.de",
+                    info: "Sei wer du bist, wenns schlecht läuft: es gibt Friedrich ^^",
                    
                 }
     
