c2d88fd87499ac2175925c4f95b3b779426bff93
Lukas Baginski <lukas.baginski@ambilstein.de>
2021-02-16 09:43:33 +0100

    More or less good looking chat.

--- gfos_frontend/frontend2/src/components/Chat.vue
+++ gfos_frontend/frontend2/src/components/Chat.vue
@@ -1,23 +1,23 @@
 <template>
     <v-container>
-        <div>
-            <div v-for="item in testitems" :key="item">
-                <div v-if="item.author === 'me'" style="border-width: 1px; border-style: solid; max-width: 400px; height:200px; float: right; position: relative;">
+        <div style="background-color: #202b36; border-radius: 15px; padding: 16px;">
+            <div v-for="item in testitems" :key="item" style="height: 100%;">
+                <div v-if="item.author === 'me'" style="background-color: #2b5278; border-width: 1px; border-style: solid; border-radius: 15px; max-width: 400px; height: auto; position: relative; left: 50%;">
                     <v-list-item three-line>
                         <v-list-item-content>
-                            <v-list-item-subtitle>{{item.msg}}</v-list-item-subtitle><br>
-                            <v-list-item-subtitle>{{item.time}}</v-list-item-subtitle>
+                            <p style="color: white; white-space: pre-line;">{{item.msg}}</p>
+                            <v-list-item-subtitle style="color: white;">{{item.time}}</v-list-item-subtitle>
                         </v-list-item-content>
                     </v-list-item>
                 </div>
-                <div v-else style="border-width: 1px; border-style: solid; height: 200px; max-width: 400px; position: relative;">
+                <div v-else style="background-color: #252d30; border-width: 1px; border-style: solid; border-radius: 15px; max-width: 400px; height: auto; position: relative;">
                     <v-list-item three-line>
                         <v-list-item-content>
-                            <div class="overline mb-4">
+                            <div class="overline mb-4" style="color: white;">
                                 {{item.author}}
                             </div>
-                            <v-list-item-subtitle>{{item.msg}}</v-list-item-subtitle><br>
-                            <v-list-item-subtitle>{{item.time}}</v-list-item-subtitle>
+                            <p style="color: white; white-space: pre-line;">{{item.msg}}</p>
+                            <v-list-item-subtitle style="color: white;">{{item.time}}</v-list-item-subtitle>
                         </v-list-item-content>
                     </v-list-item>
                 </div>
@@ -32,7 +32,7 @@
     name: 'Chat',
 
     data: () => ({
-      testitems: [{"author": "me", "msg": "Hallo Welt!", "time": "10:04"}, {"author": "notme", "msg": "Guten Morgen.", "time": "10:05"}]
+      testitems: [{"author": "me", "msg": "Hallo Welt!", "time": "10:04"}, {"author": "notme", "msg": "Guten Morgen.", "time": "10:05"}, {"author": "notme", "msg": "Eine zweite Nachricht meinerseits.", "time": "10:06"}, {"author": "me", "msg": "Zum Schluss folgt eine Nachricht von mir. Am besten mit mehreren Zeilen! Hier folgt ein weiterer Text, um die Zeilen zu füllen. Damit kann überprüft werden, ob das Design richtig funktioniert und nicht eine fixe Höhe hat.\n\nDies sollte der letzte Satz sein, das Textfeld sollte nun mehrere Zeilen haben. Einen schönen Tag noch!", "time": "10:06"}, {"author": "me", "msg": "Hallo Welt!", "time": "10:04"}, {"author": "notme", "msg": "Guten Morgen.", "time": "10:05"}, {"author": "notme", "msg": "Eine zweite Nachricht meinerseits.", "time": "10:06"}, {"author": "me", "msg": "Zum Schluss folgt eine Nachricht von mir. Am besten mit mehreren Zeilen! Hier folgt ein weiterer Text, um die Zeilen zu füllen. Damit kann überprüft werden, ob das Design richtig funktioniert und nicht eine fixe Höhe hat.\n\nDies sollte der letzte Satz sein, das Textfeld sollte nun mehrere Zeilen haben. Einen schönen Tag noch!", "time": "10:06"}]
     }),
   }
 </script>
