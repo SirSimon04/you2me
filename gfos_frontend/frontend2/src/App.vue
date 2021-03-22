ef006d0d84aab29351144768339db28341ddd6a3
JET3141 <jan.thomas@gymnasium-essen-werden.de>
2021-03-22 22:26:57 +0100

    Änderungen an der App, Registrieren und Welcome

--- gfos_frontend/frontend2/src/App.vue
+++ gfos_frontend/frontend2/src/App.vue
@@ -47,11 +47,11 @@ export default {
     },
     
     data: () => ({
-        welcome_vue_zeigen: true,
-        app_vue_registrieren_zeigen: false,
+        welcome_vue_zeigen: false,
+        app_vue_registrieren_zeigen: true,
         app_vue_settings_zeigen: false,
         app_vue_chatliste_zeigen: false,
-        app_vue_chat_zeigen: true,
+        app_vue_chat_zeigen: false,
         user: Registrieren.benutzername,
     }),    
 };
