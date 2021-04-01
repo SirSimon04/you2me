3cdc9a2532dbf7deb39cd780e1e987011c13faa9
LukasBaginski <lukas.baginski@ambilstein.de>
2021-04-01 20:29:33 +0200

    Added sendable message and animations

--- gfos_frontend/frontend2/src/components/Chat.vue
+++ gfos_frontend/frontend2/src/components/Chat.vue
@@ -29,6 +29,39 @@
             </div>
             -->
         </div>
+        <div style="
+                background-color: rgb(27, 66, 104);
+                border-radius: 20px;
+                min-height: 32px;
+                max-height: 256px;
+            ">
+            <textarea
+                style="
+                    margin: 8px;
+                    position: relative;
+                    top: calc(50% - 16px);
+                    color: white;
+                    width: 90%;
+                    max-height: 240px;
+                    min-height: 32px;
+                    
+                    overflow: auto;
+                "
+                id="sendMessage_Area"
+                maxlength="1024"
+                oninput="
+                    function ch(elem) {
+                        elem.style.height = '48px';
+                        elem.style.height = elem.scrollHeight + 'px';
+                    }
+                    ch(this);
+                "
+                placeholder="Schreibe eine Nachricht..."
+                wrap="hard"
+            >
+            </textarea>
+            <img onclick="window.sendMessage();" style="position: relative; right: -4px; bottom: 4px;" width="32" height="32" id="sendMessage_Button" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAGfSURBVEiJrdQvTxxRFAXw32SzBr4FHggSi6hpUrMYiimQ9HOgIHj4FljEQoIEWoFD0CYkVa0rbYLlVcydzWM6s7M7MMkTc/+cc+49M69IKXnLpyiKIT7gMwZSSm9ysIQD/ESKM34t6BAjjPGM33jKCEavUXuIXwF0hU84ivenmGQ4r9pNnIfaRxxjOfL7AX4UkxyklPRRe40dLGQ1Ffh+TPKMpVaCKWpXGmon4PF+hfEk36H2Bru52g7w5crcrGai9iLU/sEJVjtW9wI8YschbpgTfInCB+xhcQZfmsAXYpWHtVpb+BHqT7E2L3jEd3JzX3gQa9rD92g+w/qs4JG7xvl/8VrRANu4C6BLbMwAvhK5zakEWUOhvAJuo/G+DbzN3KkEteb3+Ib7lnyjuTMTZCSpWlctt9tkbnWK6lPqeoqiuMVjSmmjFr/B35TSu8bGWSYIEaOYYj2LrbaZO9eKMuPvcJbFTtrMnZsgALdD8RoWlddKo7l9CQbKn/FU+WO2mtuLIEgq4AdcdNb3IBgq766p5vYmCJKP+DrN3Or8A60pb5tuGI4XAAAAAElFTkSuQmCC">
+        </div>
     </v-container>
 </template>
 
@@ -67,8 +100,9 @@ export default {
             });
         }
 
-        function testPost2() {
-            var current = new Date();
+        function sendMessage() {
+            var content = document.getElementById('sendMessage_Area').text;
+            var currentMillis = new Date.currentMillis;
             fetch('http://' + IP_ADDRESS + ':8080/GFOS/daten/nachricht/add/1', {
                 method: 'POST',
                 headers: {
@@ -76,10 +110,10 @@ export default {
                     'Content-Type': 'text/plain'
                 },
                 body: JSON.stringify({
-                    senderid: 2,
-                    inhalt: 'Hallo Welt',
-                    datumuhrzeit: current.toLocaleString(),
-                    chatid: 1,
+                    senderid: CURRENT_USER_ID,
+                    inhalt: content,
+                    datumuhrzeit: currentMillis,
+                    chatid: CURRENT_CHAT_ID,
                 })
             }).then(response => {
                 response.clone();
@@ -152,6 +186,7 @@ export default {
 
             document.getElementById('chatcontainer').innerHTML = '';
 
+            // Nachrichten mit dieser Animation einfügen: https://www.w3schools.com/css/tryit.asp?filename=trycss3_animation_speed
             function loadMessages() { // Reload all messages (will be called if a newer message is available)
                 fetch('http://' + IP_ADDRESS + ':8080/GFOS/daten/nachricht/chat/' + CURRENT_CHAT_ID + '/1').then(response => {
                     if (response.status !== 200) {
@@ -168,7 +203,7 @@ export default {
                         for (var i=0; i<msgs.length; i++) {
                             var data = msgs[i];
                             var elem = '';
-                            // Add icon for planned message (if sent by me: icon; if sent by other: show if date is not a future date)
+                            // Add icon for planned message (ifÖsent by me: icon; if sent by other: show if date is not a future date)
                             if (data['senderid'] === CURRENT_USER_ID) elem = '<div class="singlemsgcontainer" style="height: 100%;"><div style="background-color: #2B5278; border-width: 1px; border-style: solid; border-top-left-radius: 15px; border-top-right-radius: 15px; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px; max-width: 400px; height: auto; position: relative; right: calc(-100% + 400px);"><div tabindex="-1" class="v-list-item v-list-item--three-line theme--light"><div class="v-list-item__content"><p style="color: white; white-space: pre-line;">' + data["inhalt"] + '</p><div class="v-list-item__subtitle" style="color: white; margin-top: 6px;">' + data["datumuhrzeit"] + '</div></div></div></div><br></div>';
                             else elem = '<div class="singlemsgcontainer" style="height: 100%;"><div style="background-color: #182533; border-width: 1px; border-style: solid; border-radius: 15px; max-width: 400px; height: auto; position: relative;"><div tabindex="-1" class="v-list-item v-list-item--three-line theme--light"><div class="v-list-item__content"><div class="overline mb-2" style="color: white;">' + data["sender"] + '</div><p style="color: white; white-space: pre-line;">' + data["inhalt"] + '</p><div class="v-list-item__subtitle" style="color: white; margin-top: 6px;">' + data["datumuhrzeit"] + '</div></div></div></div><br></div>';
                             cc.innerHTML += elem;
@@ -231,5 +266,81 @@ export default {
 
         }); // EventBus.$on('OPENCHAT')
     }, // mounted()
-  }
+}
+window.sendMessage = function sendMessage() {
+    var content = document.getElementById('sendMessage_Area').value;
+    document.getElementById('sendMessage_Area').value = '';
+    var currentMillis = new Date().getTime();
+
+    var time = 0;
+    var button = document.getElementById('sendMessage_Button');
+    var animationDuration = 0.75;
+    function F(t) {
+        var a = 100;
+        var pot = t / animationDuration;
+        return Math.pow(a, pot);
+    }
+    function f(t) {
+        var pot = (4 * t) / 3;
+        var z = Math.log1p(100) * Math.pow(100, pot);
+        return (4 * z) / 3;
+    }
+    var sendAnim = setInterval(function() {
+        var x = F(time);
+        var r = (Math.atan(f(time)) * 180) / Math.PI * 1.025;
+
+        var right = -4 - x;
+        var bottom = 4 + x;
+
+        button.style = `
+        position: relative;
+        right: ` + right + `px;
+        bottom: ` + bottom + `px;
+        transform: rotate(` + (-r + 82.78883453737954) + `deg);
+        `;
+
+        time += 0.01;
+        if (time > animationDuration) {
+            clearInterval(sendAnim);
+            time = -0.25;
+            var resetDuration = 0.5;
+            var resetAnim = setInterval(function() {
+
+                var opacity = time / resetDuration;
+
+                button.style = `
+                position: relative;
+                right: -4px;
+                bottom: 4px;
+                opacity: ` + opacity + `;
+                transform: rotate(0deg);
+                `;
+
+                time += 0.01;
+                if (time > resetDuration) {
+                    clearInterval(resetAnim);
+                }
+            }, 10);
+        }
+    }, 10);
+
+    fetch('http://' + this.IP_ADDRESS + ':8080/GFOS/daten/nachricht/add/1', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'text/plain'
+        },
+        body: JSON.stringify({
+            senderid: this.CURRENT_USER_ID,
+            inhalt: content,
+            datumuhrzeit: currentMillis,
+            chatid: this.CURRENT_CHAT_ID,
+        })
+    }).then(response => {
+        response.clone();
+        response.text().then(content => {
+            console.warn(content);
+        }); 
+    });
+}
 </script>
