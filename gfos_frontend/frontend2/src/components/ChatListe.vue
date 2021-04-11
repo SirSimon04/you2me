be1ea85fdbd5a021aea50ec1f51aef19dc08c153
Lukas Baginski <lukas.baginski@ambilstein.de>
2021-04-11 14:27:21 +0200

    Chat badge

--- gfos_frontend/frontend2/src/components/ChatListe.vue
+++ gfos_frontend/frontend2/src/components/ChatListe.vue
@@ -65,7 +65,13 @@ export default {
 
                 var profilePicture = (data['profilbild'] === undefined) ? imageContainer.getValue('USER') : data['profilbild']['base64'];
                 var pinnedIcon = (data['ispinned'] === undefined) ? '' : '<img style="z-index: 100; position: relative; float: right; top: calc(50% - 4px); right: 8px;" height="24px" width="24px" src="' + imageContainer.getValue("PIN") + '">'
-                var unreadBadge = (data['nnew'] === 0) ? '' : '<div style="z-index: 100; position: relative; float: right; top: calc(50% - 4px); right: 8px; width: 24px; height: 24px; background-color: pink; border-radius: 100%;">' + data["nnew"] + '</div>';
+                var unreadBadge = (data['nnew'] === 0) ? '' : `
+                <div >
+                    <div style="z-index: 100; position: relative; float: right; top: calc(50% - 24px); left: -8px; width: 24px; height: 24px; background-color: pink; border-radius: 100%;">
+                        <div style="position: relative; left: 7px;">` + data["nnew"] + `</div>
+                    </div>
+                </div>
+                `;
                 var elem = `
                 <div onclick="window.openChat(` + chatid + ', ' + window.CURRENT_USER_ID + `);" class="mx-auto v-card v-sheet theme--light" style="height: 80px; max-width: 400px; background-color: rgb(23, 33, 43); border-radius: 0px;">
                     ` + unreadBadge + pinnedIcon + `
