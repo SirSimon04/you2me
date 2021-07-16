fc0ad55277cad79c5a5939226c1645effa6343ca
SirSimon04 <simi@engelnetz.de>
2021-07-17 00:58:39 +0200

    Fixed bug with user profile pic with changing getOtherUserPhotoUrls

--- lib/services/user_services/GeneralUserService.dart
+++ lib/services/user_services/GeneralUserService.dart
@@ -1,14 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dispuatio/models/chat_model.dart';
 
 class GeneralUserService {
   static FirebaseAuth _auth = FirebaseAuth.instance;
 
-  static String getOtherUserFotoUrl(chat) {
-    if (chat.members[0].split("|")[0] == _auth.currentUser?.uid) {
-      return chat.fotoUrls[1];
-    } else if (chat.members[1].split("|")[0] == _auth.currentUser?.uid) {
+  static String getOtherUserFotoUrl(ChatModel chat) {
+    // if (chat.members[0].split("|")[0] == _auth.currentUser?.uid) {
+    //   return chat.fotoUrls[1];
+    // } else if (chat.members[1].split("|")[0] == _auth.currentUser?.uid) {
+    //   return chat.fotoUrls[1];
+    // }
+    // return "";
+
+    if (chat.fotoUrls[1] == _auth.currentUser?.photoURL) {
+      return chat.fotoUrls[0];
+    } else {
       return chat.fotoUrls[1];
     }
-    return "";
   }
 }
