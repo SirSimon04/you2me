4535577880e47375b37c55ae483e7d8a8181346b
SirSimon04 <simi@engelnetz.de>
2021-10-25 21:40:36 +0200

    changed bundleid to work again

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_dispuatio/screens/chatting/chatlist/chat_list_screen.dart';
@@ -8,7 +9,7 @@ import 'package:flutter_dispuatio/services/user_services/user_firebase_service.d
 import 'package:flutter_dispuatio/themes.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:device_preview/plugins.dart';
 import 'screens/friends/friends.dart';
 import 'screens/settings/settings.dart';
 
@@ -26,9 +27,23 @@ void main() async {
   await Firebase.initializeApp();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString("email");
-  runApp(MyApp(
-    loggedIn: email != null,
-  ));
+
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(
+        loggedIn: email != null,
+      ),
+      plugins: [
+        const ScreenshotPlugin(),
+        const FileExplorerPlugin(),
+        const SharedPreferencesExplorerPlugin(),
+      ],
+    ),
+  );
+  // runApp(MyApp(
+  //   loggedIn: email != null,
+  // ));
 }
 
 class MyApp extends StatelessWidget {
@@ -42,8 +57,10 @@ class MyApp extends StatelessWidget {
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
-      theme: lightThemeData(context),
-      darkTheme: darkThemeData(context),
+      // theme: lightThemeData(context),
+      // darkTheme: darkThemeData(context),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       // home: WelcomeScreen(),
       home: loggedIn ? MyHomePage() : LoginScreen(),
       debugShowCheckedModeBanner: false,
