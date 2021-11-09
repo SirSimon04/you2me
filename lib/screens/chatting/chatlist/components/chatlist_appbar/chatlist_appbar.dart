0b9a8991eba3e10ff846a478e055c002c6cea32c
SirSimon04 <simi@engelnetz.de>
2021-11-09 21:29:15 +0100

    added button for settings, only sowing when desktop or tablet, changed pos

--- lib/screens/chatting/chatlist/components/chatlist_appbar/chatlist_appbar.dart
+++ lib/screens/chatting/chatlist/components/chatlist_appbar/chatlist_appbar.dart
@@ -11,69 +11,64 @@ getChatListAppBar({
   required BuildContext context,
 }) {
   return AppBar(
-      automaticallyImplyLeading: isArchiveOpen,
-      title: isArchiveOpen
-          ? Text(
-              "Archivierte Chats",
-              style: TextStyle(color: Colors.white),
-            )
-          : Text(
-              "Chats",
-              style: TextStyle(
-                color: Colors.white,
-              ),
+    automaticallyImplyLeading: isArchiveOpen,
+    title: isArchiveOpen
+        ? Text(
+            "Archivierte Chats",
+            style: TextStyle(color: Colors.white),
+          )
+        : Text(
+            "Chats",
+            style: TextStyle(
+              color: Colors.white,
             ),
-      actions: [
-        !isArchiveOpen
-            ? IconButton(
-                onPressed: () {
-                  onArchivePress();
-                },
-                icon: Icon(
-                  FontAwesomeIcons.solidFolderOpen,
-                  color: Colors.white,
-                ),
-              )
-            : Container(),
-        Padding(
-          padding: const EdgeInsets.only(right: 8.0),
-          child: IconButton(
-              onPressed: () => createGroupModalMaterialBottomSheet1(context),
-              icon: Icon(
-                FontAwesomeIcons.users,
-                color: Colors.white,
-              )),
-        )
-      ],
-      leading: (Responsive.isDesktop(context) ||
-              Responsive.isTablet(context) ||
-              isArchiveOpen)
-          ? getLeadingRow(context, isArchiveOpen, onArchivePress)
-          : null);
-}
-
-getLeadingRow(context, isArchiveOpen, onArchivePress) {
-  return Row(
-    children: [
-      if (Responsive.isDesktop(context) || Responsive.isTablet(context))
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Icon(
-            FontAwesomeIcons.hamburger,
-            color: Colors.white,
           ),
-        )
-      else
-        Container(),
-      isArchiveOpen
+    actions: [
+      !isArchiveOpen
           ? IconButton(
+              onPressed: () {
+                onArchivePress();
+              },
               icon: Icon(
-                FontAwesomeIcons.arrowLeft,
+                FontAwesomeIcons.solidFolderOpen,
                 color: Colors.white,
               ),
-              onPressed: onArchivePress,
             )
           : Container(),
+      Padding(
+        padding: const EdgeInsets.only(right: 8.0),
+        child: IconButton(
+          onPressed: () => createGroupModalMaterialBottomSheet1(context),
+          icon: Icon(
+            FontAwesomeIcons.users,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(right: 8.0),
+        child: IconButton(
+          icon: Icon(
+            FontAwesomeIcons.ellipsisV,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+      )
     ],
+    leading: isArchiveOpen
+        ? Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Expanded(
+              child: IconButton(
+                icon: Icon(
+                  FontAwesomeIcons.arrowLeft,
+                  color: Colors.white,
+                ),
+                onPressed: onArchivePress,
+              ),
+            ),
+          )
+        : null,
   );
 }
